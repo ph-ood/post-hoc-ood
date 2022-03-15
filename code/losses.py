1f7b5fcd005990ceac46ad03928696304a68aa8d
Harshit Varma <varmaharshit2@gmail.com>
2022-03-15 12:43:38 +0530

    added a loss

--- code/losses.py
+++ code/losses.py
@@ -24,3 +24,24 @@ class DualMarginLoss(nn.Module):
         loss = loss_discr + self.alpha*loss_energy
 
         return loss
+
+class HarmonicEnergyLoss(nn.Module):
+
+    def __init__(self, T, m_i, m_o, alpha):
+        super(HarmonicEnergyLoss, self).__init__()
+        self.T = T
+        self.alpha = alpha
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, logits, labels, logits_ft):
+        
+        loss_discr = self.criterion(logits, labels)
+        
+        e_i = sc.energyScore(logits, self.T) # [b,]
+        e_o = sc.energyScore(logits_ft, self.T) # [b_ft,]
+        
+        loss_energy = -(2*e_o)/(1 + e_o*e_i)
+
+        loss = loss_discr + self.alpha*loss_energy.mean()
+
+        return loss
