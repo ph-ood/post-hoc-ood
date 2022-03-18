6fd04f4e294f1050d47f3e1e34556f4d0dd22fc3
Harshit Varma <varmaharshit2@gmail.com>
2022-03-18 14:09:30 +0530

    added a score

--- code/scores.py
+++ code/scores.py
@@ -6,6 +6,11 @@ def softmaxScore(logits):
     s, _ = (F.softmax(logits, dim = -1)).max(dim = -1) # [b,]
     return s
 
+def maxLogitScore(logits):
+    # logits: [b, n_classes]
+    s, _ = torch.exp(logits.max(dim = -1)) # [b,]
+    return s
+
 def energyScore(logits, T = 1):
     # logits: [b, n_classes]
     s = T*torch.logsumexp(logits / T, dim = -1)
